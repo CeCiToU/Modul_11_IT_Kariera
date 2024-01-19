08b8b2ff92ed452d0cc266c01629d4a36a9fe4be
Tsvetoslav <cveti3060@gmail.com>
2024-01-19 21:08:38 +0200

    Internet and sockets done and sockets exercise done.

---
+++ Internet_And_Sockets/ChatClient.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the local machine's host entry
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+
+            // Obtain the first IP address from the list of addresses associated with the host
+            IPAddress iPAddress = ipHostInfo.AddressList[0];
+
+            // Create a remote end point by combining the obtained IP address with a specific port (in this case, port 11000)
+            IPEndPoint remoteEP = new IPEndPoint(iPAddress, port: 11000);
+
+            // Create a socket for sending data over a stream using TCP protocol
+            Socket sender = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Connect the sender socket to the remote end point
+                sender.Connect(remoteEP);
+
+                // Display a message indicating successful connection
+                Console.WriteLine($"Socket connected to {sender.RemoteEndPoint.ToString()}");
+
+                // Continue sending messages until "exit" is entered
+                while (true)
+                {
+                    // Prompt user for input
+                    Console.Write(">");
+
+                    // Read user input
+                    string message = Console.ReadLine();
+
+                    // Convert the message to a byte array and include "<EOF>" tag
+                    byte[] msg = Encoding.ASCII.GetBytes(s: message + "<EOF>");
+
+                    // Send the byte array over the socket
+                    int bytesSent = sender.Send(msg);
+
+                    // Check if the entered message is "exit" to break out of the loop
+                    if (message == "exit")
+                    {
+                        break;
+                    }
+                }
+
+                // Shutdown and close the sender socket after sending messages
+                sender.Shutdown(how: SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception ex)
+            {
+                // Print any exception messages to the console
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Internet_And_Sockets/ChatSever.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve the local machine's host name
+            string hostName = Dns.GetHostName();
+
+            // Retrieve information about the host using the host name
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+
+            // Obtain the first IP address from the list of addresses associated with the host
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            // Create a local end point by combining the obtained IP address with a specific port (in this case, port 11000)
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port: 11000);
+
+            // Create a socket that is listening whenever there is a package received
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                byte[] buffer = new byte[1024];
+
+                // Bind the socket to the local end point
+                listener.Bind(localEndPoint);
+
+                // Start listening with a backlog of 100 pending connections
+                listener.Listen(backlog: 100);
+
+                // Accept incoming connection and create a new socket for handling communication
+                Socket handle = listener.Accept();
+
+                while (true)
+                {
+                    string message = "";
+
+                    while (true)
+                    {
+                        // Receive data into the buffer
+                        int messageSize = handle.Receive(buffer);
+
+                        // Convert the received data to a string
+                        message += Encoding.ASCII.GetString(buffer, index: 0, count: messageSize);
+
+                        // Check if the message contains "<EOF>" indicating the end of the message 'end of file'
+                        if (message.Contains("<EOF>"))
+                        {
+                            // Remove the "<EOF>" tag from the message
+                            message = message.Replace(oldValue: "<EOF>", newValue: "");
+                            break;
+                        }
+                    }
+
+                    // Print the received message to the console
+                    Console.WriteLine("> " + message);
+
+                    // Check if the received message is "exit"
+                    if (message == "exit")
+                    {
+                        // Shutdown and close the socket
+                        handle.Shutdown(how: SocketShutdown.Both);
+                        handle.Close();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Print any exception messages to the console
+                Console.WriteLine(ex.Message);
+            }
+
+            // Clear the console and display a goodbye message
+            Console.Clear();
+            Console.WriteLine("Goodbye");
+
+            // Wait for a key press before exiting the program
+            Console.ReadKey(intercept: true);
+        }
+    }
+}
